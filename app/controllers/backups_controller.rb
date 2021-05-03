cba39f50b51ea5b3f0984a25aa09753f173d7476
Steven Price <steve@techrockstars.com>
2021-05-03 00:06:47 -0700

    Added new columns

--- app/controllers/backups_controller.rb
+++ app/controllers/backups_controller.rb
@@ -4,8 +4,56 @@ class BackupsController < ApplicationController
   def upload
     backstage = Gsheet.new("Backstage App", "Devices")
     backstage.unique_cells("Device Name")
-    data = params.permit(:device_name, :company_name, :atera_id, :last_backup_date, :backups_completed, :backups_started, :backups_failed, :api_key, :hdd_keys)
-    backstage.insert_rows([ data[:device_name], data[:company_name], 0,0,data[:atera_id], data[:last_backup_date], data[:backups_started], data[:backups_completed], data[:backup_faileds], "", Time.zone.now, "", data[:hdd_keys]])
+    data = params.permit(
+      :device_name, 
+      :company_name, 
+      :atera_id, 
+      :last_backup_date, 
+      :backups_completed, 
+      :backups_started, 
+      :backups_failed, 
+      :api_key, 
+      :hdd_keys,
+      :firewall_on,
+      :secured,
+      :bios_date,
+      :windows_ver,
+      :chef_complete,
+      :drive_status,
+      :drive_speed,
+      :computer_num,
+      :computer_score,
+      :user_policy_updated,
+      :computer_policy_updated,
+      :z2ls_version
+    )
+    backstage.insert_rows([ 
+                          data[:device_name], 
+                          data[:company_name], 
+                          0,
+                          0,
+                          data[:atera_id], 
+                          data[:last_backup_date], 
+                          data[:backups_started], 
+                          data[:backups_completed], 
+                          data[:backup_faileds], 
+                          "", 
+                          Time.zone.now, 
+                          "", 
+                          data[:hdd_keys]
+                          data[:firewall_on]
+                          data[:secured]
+                          data[:bios_date]
+                          data[:windows_ver]
+                          data[:chef_complete]
+                          data[:drive_status]
+                          data[:drive_speed]
+                          data[:computer_num]
+                          data[:computer_score]
+                          data[:uesr_policy_updated]
+                          data[:computer_policy_updated]
+                          data[:z2ls_version]
+    ])
     backstage.save
     
     #head :ok #Commented this out because jets uses rails 5
